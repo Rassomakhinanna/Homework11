1982d9efa91d5199408efda0738aebf39c64833f
annarassomahina <pupkinppppp@gmail.com>
2022-08-14 20:24:21 +0400

    HW11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author VictorHugo = new Author("Victor Hugo");
+        Book LM = new Book("Les Miserables", 1862, VictorHugo);
+        Book LM2 = new Book("Les Miserables", 1862, VictorHugo);
+        System.out.println(LM);
+        System.out.println(LM.equals(LM2));
     }
 }
